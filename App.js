83b8aad1fd036142597a6ccf8d3009e0268a11b8
JulioJohan <juliojohan10@gmail.com>
2022-04-02 19:30:49 -0600

    LoginYCrearCuenta

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import React from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { NavigationContainer} from '@react-navigation/native';
+import { createStackNavigator} from '@react-navigation/stack'
+const Stack = createStackNavigator();
+
+import Login from './views/Login';
+import CrearCuenta from './views/CrearCuenta';
+
+//sfc
+const App = () => {
+  return ( 
+    <>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{
+            title: "Iniciar Sesión",
+            headerShown: false
+          }}
+        />
+        <Stack.Screen
+          name="CrearCuenta"
+          component={CrearCuenta}
+          options={{
+            title: "Crear cuenta",
+            headerStyle:{
+              backgroundColor: '#800080'
+            },
+            headerTintColor: '#fff',
+            headerTitle:{
+              fontWeight: 'Bold'
+            }
+          }}
+        />
+
+      </Stack.Navigator>
+    </NavigationContainer>
+    </>
+   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+ 
+export default App;
